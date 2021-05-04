9512a88849aa7308be26e9589ea751f549c1bfc1
Rossig4 <r.silvera.ag@gmail.com>
2021-05-04 19:06:36 +0000

    casi terminado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,37 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 
 //create your first component
 export function Home() {
+	const [tablero, setTablero] = useState([
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	]);
+	const marcar = index => {
+		let tableroauxiliar = [...tablero];
+		tableroauxiliar[index] = turnoactual;
+		setTablero(tableroauxiliar);
+		cambiarturno();
+	};
+	const [turnoactual, setTurnoactual] = useState("X");
+	const cambiarturno = () => {
+		if (turnoactual === "X") {
+			setTurnoactual("O");
+		} else if (turnoactual === "O") {
+			setTurnoactual("X");
+		}
+	};
 	return (
-		<div className="flex-container">
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
-			</div>
-			<div className="mosaico">
-				<h1>x</h1>
+		<div className="fondo">
+			<h1>Tic Tac Toe</h1>
+			<h2>It is X tourn</h2>
+			<div className="flex-container">
+				{tablero.map((item, index) => {
+					return (
+						<div
+							key={index}
+							onClick={() => {
+								marcar(index);
+							}}
+							className="mosaico">
+							<h1>{item}</h1>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
